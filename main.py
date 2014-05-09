a2bcbd0cf2e048f868bb608029e5f9cda747e2de
Kevin Rasmussen <kj_529@hotmail.com>
2014-05-09 00:20:34 -0700

    Now have API pulling from DB and Calender pulling from API

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, g, json
 import sqlite3
 
 app = Flask(__name__)
@@ -37,30 +37,71 @@ def commit_db():
 	if db is not None:
 		db.commit()
 
+def db_to_json(dbresponse):
+	'''
+	Takes a list of truples from the dbresponse and returns json list
+	'''
+
+	listdict = []
+	for result in dbresponse:
+		listdict.append(
+			{
+				"id" : result[0],
+				"title" : result[1],
+				"description" : result[2],
+				"location" : result[3],
+				"name" : result[4],
+				"url" : result[5],
+				"start" : result[6],
+				"end" : result[7],
+			}
+		)
+	return json.dumps(listdict)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	return "The api can be found at /api"
+	return render_template("calendar.html")
 
 @app.route('/api', methods=['GET', 'POST'])
 def api():
 
 	try:
-		startTimestamp = request.args['startTimestamp']
-		endTimestamp = request.args['endTimestamp']
+		startTimestamp = request.args['start']
+		endTimestamp = request.args['end']
 	except:
-		return "No or incomplete form data received"
+		pass
+		#return "No or incomplete form data received"
 
 	# Attempt to Query DB
 	try:
-		#results = query_db("SELECT * FROM calendar WHERE datetime(start, 'unixepoch') >= datetime(%s, 'unixepoch') AND datetime(end, 'unixepoch') <= datetime(%s, 'unixepoch') ORDER BY datetime(start, 'unixepoch') ASC;" %(startTimestamp, endTimestamp))
-		results = query_db("SELECT * FROM calendar;")
-	except: #error as e:
+		#results = query_db("SELECT * FROM calendar WHERE datetime(start) >= datetime(%s) AND datetime(end) <= datetime(%s) ORDER BY datetime(start) ASC;" %(startTimestamp, endTimestamp))
+		results = query_db("SELECT * FROM calendar WHERE datetime(start) >= datetime(%s, 'unixepoch') AND datetime(end) <= datetime(%s, 'unixepoch') ORDER BY datetime(start) ASC;" %(startTimestamp, endTimestamp))
+	except:
 		raise
-		#return "Error: %s" %(e.value)
 
-	return "Start Time: %s<br>End Time: %s<br> %s" %(startTimestamp, endTimestamp, results)
 
-	
+	'''''
+	testresult = '''
+	[
+		{
+			"id" : "1",
+			"title" : "Test Event 1",
+			"description" : "This is the first test event",
+			"start" : "2014-05-06 05:30:26",
+			"end" : "2014-05-07 05:30:26",
+			"location" : "Acme Hall, Room 101",
+			"name" : "John Smith"
+		}
+	]
+	'''
+	'''''
+
+	#return testresult
+
+	return(db_to_json(results))
+
+	#return jsonify({'results':results})
+
 
 if __name__ == '__main__':
 	app.run(debug = True)
